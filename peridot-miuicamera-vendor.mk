53607573c2a2697999a4cc4aa0a5d032a79d75d4
Garten <brilliangarten@gmail.com>
2026-02-25 09:22:27 +0700

    Update peridot-miuicamera-vendor.mk

--- peridot-miuicamera-vendor.mk
+++ peridot-miuicamera-vendor.mk
@@ -6,9 +6,9 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/peridot-miuicamera
 
 PRODUCT_PACKAGES += \
+    MiuiCamera \
     libcamera_algoup_jni.xiaomi \
     libcamera_mianode_jni.xiaomi \
     libgui-xiaomi \
     libmicampostproc_client \
-    vendor.xiaomi.hardware.campostproc@1.0_system \
-    MiuiCamera
+    vendor.xiaomi.hardware.campostproc@1.0
